dce24f015df4db05ec9f68eb002ccce5ead3d35e
aashan007 <sarathchandran1995@gmail.com>
2021-03-22 18:43:59 +0530

    added uuid

--- Server/src/app.ts
+++ Server/src/app.ts
@@ -3,10 +3,24 @@ import {generateUUID} from './generateUUID'
 
 const app = express();
 const port = 3000;
-app.get('/', (req, res) => {
-  let uuid:string = generateUUID();
+
+// generate uuid and send the response
+app.get('/create', (req, res) => {
+  let uuid:string;
+  do
+  {
+    uuid = generateUUID();
+  }while(false);
+  //create an entry in the database with the uuid
+  
   res.send(uuid);
 });
+
+
+//update the database with the stickynotes detail
+
+
+
 app.listen(port,  () => {
   return console.log(`server is listening on ${port}`);
 });
