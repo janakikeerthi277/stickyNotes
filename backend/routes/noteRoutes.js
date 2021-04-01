be6eb23671530868b95d1549cf8b6aabd07900e6
Janaki <janakikeerthi277@users.noreply.github.com>
2021-04-01 12:58:52 +0530

    Update noteRoutes - add and delete

--- backend/routes/noteRoutes.js
+++ backend/routes/noteRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const {getAllProducts,getProductsById,createNewUUID} = require('../controller/notesController')
+const {getAllProducts,getProductsById,createNewUUID,addNote,deleteNote} = require('../controller/notesController')
 
 //@desc get all notes 
 //@route /api/notes
@@ -13,5 +13,8 @@ router.get('/:id',getProductsById);
 
 router.post('/generateuuid',createNewUUID);
 
+router.patch('/addnote/:id',addNote);
+
+router.patch('/deletenote/:id1/:id2', deleteNote);
 
 module.exports = router;
