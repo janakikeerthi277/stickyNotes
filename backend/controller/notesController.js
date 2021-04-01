6d7d0545ca114e937391086bef2f589e8b0d2667
Janaki <janakikeerthi277@users.noreply.github.com>
2021-04-01 13:00:20 +0530

    Update noteController - add and delete note

--- backend/controller/notesController.js
+++ backend/controller/notesController.js
@@ -43,9 +43,54 @@ const createNewUUID = async(req,res) =>{
     }
 }
 
+const addNote = async(req,res) => {
+    await Note.findOneAndUpdate(
+        {_id: req.params.id} ,
+        {$push : {notes : req.body}},
+        { new : true },
+      ).then(
+        () => {
+          res.status(201).json({
+            message: "New note created successfully"
+          });
+        }
+      ).catch(
+        (error) => {
+          res.status(400).json({
+            error: error
+          })
+        }
+      )
+}
+
+const deleteNote = async(req,res) => {
+    await Note.findOneAndUpdate(
+        { _id: req.params.id1 },
+        { $pull: { notes : { _id: req.params.id2} } },
+        { new: true },
+        function(err) {
+            if (err) { console.log(err) }
+        }
+      ).then(
+          () => {
+            res.status(200).json({
+            message: "Note deleted successfully!"
+          });
+        }
+      ).catch(
+          (error) => {
+            res.status(400).json({
+            error: error
+          })
+        }
+      )
+}
+
 
 module.exports ={
     getAllProducts,
     getProductsById,
-    createNewUUID
+    createNewUUID,
+    addNote,
+    deleteNote
 }
