504963b9cfe62a40ca65e1b93fd5d6d9d2792e30
Janaki <janakikeerthi277@users.noreply.github.com>
2021-04-02 13:32:06 +0530

    Updated findurl

--- backend/routes/noteRoutes.js
+++ backend/routes/noteRoutes.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
-const {getAllProducts,getProductsById,createNewUUID,addNote,deleteNote} = require('../controller/notesController')
+const {getAllProducts,getProductsById,createNewUUID,addNote,deleteNote,findUrl} = require('../controller/notesController')
 
 //@desc get all notes 
 //@route /api/notes
 router.get('/',getAllProducts)
 
+router.get('/findurl/:url',findUrl);
 
+//the below post request can be used to add data
+//router.post('/addnotesparent',addNotesParent);
 
 router.get('/:id',getProductsById);
 
