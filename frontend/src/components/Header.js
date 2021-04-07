90c3528d25173c36b1151bb937473ae2e7c6705d
Janaki Keerthi <janakikeerthi277@gmail.com>
2021-04-07 21:10:15 +0530

    Added code for Header.css Header.js with Email functionality

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,37 +1,48 @@
 import './Header.css';
 import {Link} from 'react-router-dom'
+import * as emailjs from 'emailjs-com';
+import { useState } from 'react';
+import axios from 'axios';
 
 const Header = () =>{
+
+        const [email,setEmail] = useState('')
+        const handleSubmit = () => {
+            
+         console.log(email);
+         emailjs.send("service_o6nx135","template_7w9gysj",{
+            to_email : email,
+            message: `${window.location}`,
+            },"user_2YdDi56FH42QWhKtYnkcA").then(function(response) {
+                console.log('SUCCESS!', response.status, response.text);
+             }, function(error) {
+                console.log('FAILED...', error);
+             });
+        }
+
+        const deleteurl = () => {
+            axios.delete(`http://localhost:5000/api/notes/deleteurl${window.location.pathname}`)
+            // props.history.push('http://localhost:3000')
+            // props.history.goBack();
+        }
+  
     return (
         <div className="navbar">
             {/*  Logo*/}
 
-            <div className="header_logo">
+            <div className="header_logo" style={{display:"flex", justifyContent:"center"}}>
                 <h2>Sticky Notes</h2>
+                <button className="btn-header delete gray" onClick={deleteurl} style={{ marginLeft:"80%"}}>Delete</button>
             </div>
 
             {/* Root Actions Button */}
 
-            <ul className="header_links">
-                <li>
-                {/*
-                    <Link to="/cart">
-                    Cart
-                    <span className="cartlogo_badge">0</span>
-                    </Link>
-
-                    <li>
-                    <Link to="/cart">
-                    Cart
-                    <span className="cartlogo_badge">0</span>
-                    </Link>
-                    </li>
-
-
-                */}
-
-                </li>
-            </ul>
+            <div className="header_links" style={{display:"flex", justifyContent:"center"}}>
+                    <input onChange = {event =>setEmail(event.target.value)} id="email" placeholder="enter your email here"/>
+                    <button className="btn-header gray" onClick={handleSubmit}>Send</button>
+                    <input id="search" placeholder="search for a note here"/>
+                    <button className="btn-header gray">Search</button>              
+            </div>
 
         </div>
     )
--- frontend/src/screens/NoteScreen.js
+++ frontend/src/screens/NoteScreen.js
@@ -1,9 +1,10 @@
 import './NoteScreen.css'
 import Note from '../components/Note'
+import Header from '../components/Header'
 const NoteScreen = () => {
     return (
         <div className="notescreen">
-
+                <Header/>
                 <Note/>
         </div>
     )
