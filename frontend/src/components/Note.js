c1524b181ebe5bbdc38c842ee87e5e3d75f24158
Janaki Keerthi <janakikeerthi277@gmail.com>
2021-04-10 19:17:09 +0530

    Bug Fix : Delete note

--- frontend/src/components/Note.js
+++ frontend/src/components/Note.js
@@ -26,15 +26,19 @@ const notesReducer = (prevState, action) => {
         totalNotes: prevState.notes.length - 1,
         notes: prevState.notes.filter(note => note.id !== action.payload.id)
       };
-      axios.patch(`http://localhost:5000/api/notes/deletenote${window.location.pathname}/${action.payload.id}`
-                  ).then(res=>{
-                      console.log(res.data)}
-                  )
+      
       console.log('After delete note', newState);
       return newState;
     }
   }
 }
+const delNote = (id) => {
+  axios.patch(`http://localhost:5000/api/notes/deletenote${window.location.pathname}/${id}`
+                 ).then(res=>{
+                      console.log(res.data)}
+                  )
+}
+
 const Note = (props) => {
   const [noteInput,setNoteInput] = useState('');
   
@@ -136,7 +140,9 @@ const Note = (props) => {
             onDragEnd={dropNote}
             key={note.id}>
 
-            <div onClick={() => dispatch({ type: 'DELETE_NOTE', payload: note })} className="close">
+            <div onClick={() => {
+              dispatch({ type: 'DELETE_NOTE', payload: note });
+              delNote(note.id);}} className="close">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
                 <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" />
               </svg>
